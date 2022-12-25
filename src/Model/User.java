40d018206d9268649eba3fd636c0c4e9648d297a
beste küçük <peribeste48@gmail.com>
2022-12-25 23:08:19 +0300

    added

--- src/Model/User.java
+++ src/Model/User.java
@@ -5,19 +5,71 @@ public class User {
 
 
     }
-
+    public Model.UserType UserType;
     public int id;
     public String ad;
     public String soyad;
-
     public String sifre;
-    public int tcNo;
-   // public String sehir;
-    public int telNo;
-    public Model.UserType UserType;
+    public String tcNo;
+    public String telNo;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getAd() {
+        return ad;
+    }
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+
+    public String getTcNo() {
+        return tcNo;
+    }
+
+    public void setTcNo(String tcNo) {
+        this.tcNo = tcNo;
+    }
 
 
-    public User(String ad, String soyad, int tcNo, int telNo,UserType userType) {
+    public String getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+
+    // public String sehir;
+    public Model.UserType getUserType() {
+        return UserType;
+    }
+
+    public void setUserType(Model.UserType userType) {
+        UserType = userType;
+    }
+
+
+
+    public User(String ad, String soyad, String tcNo, String telNo,UserType userType) {
         super();
         this.ad = ad;
         this.soyad = soyad;
